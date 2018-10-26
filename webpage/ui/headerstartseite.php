d7ec068df38e38f079fd1ca8544214528706c96c
Moritz Klug <mk304@hdm-stuttgart.de>
2018-10-26 14:50:29 +0200

    Update webpage/ui/headerstartseite.php

--- webpage/ui/headerstartseite.php
+++ webpage/ui/headerstartseite.php
@@ -6,12 +6,13 @@ session_start();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Complete Bootstrap 4 Website Layout</title>
+    <title>Campus LogIn</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
     <link href="../../../Downloads/Bootstrap%204%20Site%20Starter%20Files/style.css" rel="stylesheet">
 </head>
 <body>
@@ -20,12 +21,7 @@ session_start();
 <nav class="navbar navbar-exband-md navbar-light bg-light sticky-top">
     <div class="container-fluid">
         <a class="navbar-brand" href="#"><img src="../bilder/logoklein.png" width="70" height="64,4"></a>
-        <ul class = "nav navbar-nav navbar-right">
-            <li><a href="#"><span class ="glyphicon glyphicon-log-in">Anmelden</a> </li>
-
-        </ul>
-
-
+        <button type="button" class="btn btn-light">Anmelden</button>
     </div>
 </nav>
 
