5e57b10912cd8297eeb5796af2dcd9eb48c14315
Moritz Klug <mk304@hdm-stuttgart.de>
2018-10-26 16:31:30 +0200

    jj

--- webpage/home/Startseite.php
+++ webpage/home/Startseite.php
@@ -1,9 +1,15 @@
 <?php
 include_once '../ui/headerstartseite.php';
-
 ?>
 
+<slider>
+    <slide><p>Slide 1</p></slide>
+    <slide><p>Slide 2</p></slide>
+    <slide><p>Slide 3</p></slide>
+    <slide><p>Slide 4</p></slide>
 
+
+</slider>
 
 
 <?php
--- webpage/ui/headerstartseite.php
+++ webpage/ui/headerstartseite.php
@@ -7,6 +7,7 @@ session_start();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Campus LogIn</title>
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
@@ -14,6 +15,7 @@ session_start();
     <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
 
     <link href="../../../Downloads/Bootstrap%204%20Site%20Starter%20Files/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="../home/Startseite.css"
 </head>
 <body>
 
@@ -25,3 +27,6 @@ session_start();
     </div>
 </nav>
 
+
+</body>
+</html>
