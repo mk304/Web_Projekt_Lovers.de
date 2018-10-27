5c4df9a413a8b44fe7cdc9e21d896b7e7f343239
Moritz Klug <mk304@hdm-stuttgart.de>
2018-10-27 16:39:47 +0200

    habs

--- webpage/home/Startseite.php
+++ webpage/home/Startseite.php
@@ -1,19 +1,26 @@
 <?php
 include_once '../ui/headerstartseite.php';
 ?>
+
+
 </selection>
     <script src="src/jquery-3.3.1.min.js"></script>
     <script src="src/fullclip.min.js"></script>
     <script src="src/fullclip.js"></script>
+
         <section class="container">
+
             <div class="fullBackground"></div>
-            <div class="grid">
-                <div class="login">hjlegöfk  iwuh äqwhu ifhiöufkh ökuhf efäöwekhf äewoifh äeuwfhr äofhewäoif wqoihräo qwHRq fhwoqöieHR äweoifhq äwIORHqwäo rhqäworhq1</div>
-                <div class="textstart"> äoihwqöfkhf-hasn äöIWFB FÖweufäsufgw FHqwärkh cäshfbslfjmgj 2</div>
-            </div>
+
 
 
         </section>
+    <div class="grid-box">
+        <div class="box1">box 1</div>
+        <div class="box2">box 2</div>
+        <div class="box3">box 3</div>
+    </div>
+   
         <script>
         $('.fullBackground').fullClip({
             images: ['../bilder/hintergrundbild1.jpg', '../bilder/hintergrundbild2.jpg', '../bilder/hintergrundbild3.jpg', '../bilder/hintergrundbild4.jpg','../bilder/hintergrundbild5.jpg','../bilder/hintergrundbild6.jpg'],
