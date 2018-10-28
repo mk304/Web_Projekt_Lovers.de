7f38d0c6d677b009c2ca16dca432babe08c0665e
Moritz Klug <mk304@hdm-stuttgart.de>
2018-10-28 19:55:40 +0100

    e

---
+++ webpage/home/home.php
@@ -0,0 +1,24 @@
+<?php
+include_once '../ui/header.php';
+?>
+
+<div class="grid">
+
+    <div class="boxl">
+        <hi>Channels</hi>
+        <ul class="channel">
+            <li class="channelbox">Auslandssemester</li>
+            <li class="channelbox">Praktikum</li>
+            <li class="channelbox">Wochnungssuche</li>
+            <li class="channelbox">Jobbörese</li>
+            <li class="channelbox">Was geht heute</li>
+            <li class="channelbox">Online-Medien-Management</li>
+            <li class="channelbox">Infomationsdesign</li>
+            <li class="channelbox">Wirtschfatsinformatik</li>
+            <li class="channelbox">Mobile Medien</li>
+
+        </ul>
+    </div>
+
+    <div class="boxr"></div>
+</div>
--- webpage/ui/header.php
+++ webpage/ui/header.php
@@ -11,47 +11,44 @@ session_start();
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="../home/style.css"
 
 </head>
 
 <body>
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light ">
     <a class="navbar-brand" href="#"><img src="../bilder/logoklein.png" width="56" height="51,52"></a>
 
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse " id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
+
+            <li class="nav-item ">
+                <a class="nav-link  " href="#">
+                    <i class="fa fa-envelope-o ">
+                        <span class="badge badge-danger ">11</span>
+                    </i>
+                    Messages
                 </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
             </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
+
         </ul>
         <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
+        <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+                <img src="../bilder/user.png" width="45" height="45" alt="">
+            </a>
+        </nav>
     </div>
 </nav>
+
 
 
 </body>
