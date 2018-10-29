3ab113f79d62364f8796db5171f1405c0453c159
Moritz Klug <mk304@hdm-stuttgart.de>
2018-10-29 21:35:41 +0100

    mooved neuer header

--- webpage/home/home.php
+++ webpage/home/home.php
@@ -1,11 +1,8 @@
 <?php
-<<<<<<< HEAD
-include_once '../ui/neuerheader.php';?>
-=======
-
-include_once '../ui/header.php';
-include_once '../register/login_test.php';
-include_once 'sidebar.html';
 session_start();
+include_once 'neuerheader.php';
+
 ?>
->>>>>>> 633f082c871fdb66c1bba0195ff29b4f7ac08f32
+
+
+
