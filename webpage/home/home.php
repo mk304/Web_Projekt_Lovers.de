6bca93ecd6f0d5d7878b5ab50350cd737aebaecc
Moritz Klug <mk304@hdm-stuttgart.de>
2018-10-29 12:52:32 +0100

    Revert "fehler mit sidebar"
    This reverts commit 26404f46c1285c78380da8eed146075242026d79.

--- webpage/home/home.php
+++ webpage/home/home.php
@@ -2,6 +2,32 @@
 
 include_once '../ui/header.php';
 include_once '../register/login_test.php';
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of 26404f4... fehler mit sidebar
 session_start();
 ?>
+
+<div class="grid">
+
+    <div class="boxl">
+        <h1>Channels</h1>
+        <ul class="channel">
+
+            <li class="channelbox"><a href="#">Wochnungssuche</li>
+            <li class="channelbox"><a href="#">Jobbörese</li>
+            <li class="channelbox"><a href="#">Was geht heute</li>
+            <li class="channelbox"><a href="#">Online-Medien-Management</li>
+            <li class="channelbox"><a href="#">Infomationsdesign</li>
+            <li class="channelbox"><a href="#">Wirtschfatsinformatik</li>
+            <li class="channelbox"><a href="#">Mobile Medien</li>
+
+        </ul>
+    </div>
+
+    <div class="boxr">
+        <p>hallo</p>
+    </div>
+
+</div>
