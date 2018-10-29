75da46101d28ea56ea5524c02d0cf4cf2da4f506
carolinsommer <40471072+carolinsommer@users.noreply.github.com>
2018-10-29 20:33:08 +0100

    Posts schreiben und lesen

---
+++ webpage/register/post_input.php
@@ -0,0 +1,22 @@
+<?php
+include_once '../../userdata.php';
+session_start();
+
+//Posts in Datenbank schreiben
+$post = $_POST["post"];
+$channel = $_POST["channel"];
+$kuerzel = $_SESSION["kuerzel"];
+
+
+$pdo = new PDO ($dsn, $dbuser, $dbpass, array('charset'=>'utf8'));
+$sql = "INSERT INTO posts (kuerzel, channel, post) VALUES (?, ?, ?)";
+
+$statement = $pdo->prepare($sql);
+$statement->execute(array("$kuerzel", "$channel", "$post"));
+
+$row = $statement->fetchObject();
+
+header("Location: ../home/home.php");
+
+?>
+
