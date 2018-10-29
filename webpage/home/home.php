bc5b75b11810e2ac1e78bf4625313752db3d7263
Moritz Klug <mk304@hdm-stuttgart.de>
2018-10-29 13:06:40 +0100

    immernoch nicht....

--- webpage/home/home.php
+++ webpage/home/home.php
@@ -1,35 +1,7 @@
 <?php
 
 include_once '../ui/header.php';
-<<<<<<< HEAD
-include_once '../register/login_test.php';
-include_once 'sidebar.php';
-=======
 //include_once '../register/login_test.php';
->>>>>>> 1ac9d0abf824b8acc36b91a2d51935757a860d7b
+include_once 'sidebar.html';
 
-session_start();
 ?>
-
-<div class="grid">
-
-    <div class="boxl">
-        <h1>Channels</h1>
-        <ul class="channel">
-
-            <li class="channelbox"><a href="#">Wochnungssuche</li>
-            <li class="channelbox"><a href="#">Jobbörese</li>
-            <li class="channelbox"><a href="#">Was geht heute</li>
-            <li class="channelbox"><a href="#">Online-Medien-Management</li>
-            <li class="channelbox"><a href="#">Infomationsdesign</li>
-            <li class="channelbox"><a href="#">Wirtschfatsinformatik</li>
-            <li class="channelbox"><a href="#">Mobile Medien</li>
-
-        </ul>
-    </div>
-
-    <div class="boxr">
-        <p>hallo</p>
-    </div>
-
-</div>
