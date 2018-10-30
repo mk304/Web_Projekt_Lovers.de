f04846ee97fccb22cb2b2ae88d090b23b4f47989
Lucie Jürgens <lj025@hdm-stuttgart.de>
2018-10-30 12:08:38 +0100

    wip

--- webpage/home/neuerheader.php
+++ webpage/home/neuerheader.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <title>Campus</title>
+    <!--Beitrag erstellen -->
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <!-- Bootstrap CSS CDN -->
