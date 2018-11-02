1f91cdd7ba799181a058cdf1e2ce60e6ba7d522f
Moritz Klug <mk304@hdm-stuttgart.de>
2018-11-02 16:42:00 +0100

    Update webpage/home/home.php

--- webpage/home/home.php
+++ webpage/home/home.php
@@ -11,7 +11,6 @@ include_once 'neuerheader.php';
 ?>
 <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 
 
 <div class="container">
