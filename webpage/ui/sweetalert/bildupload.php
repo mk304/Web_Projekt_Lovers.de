ac31576f9ba8c739da7467436b170e70a5e24dc9
Moritz Klug <mk304@hdm-stuttgart.de>
2018-11-11 00:44:22 +0100

    Bild in die Datenbank fiasko
    es ist 10 vor 01:00 und ich raste aus,

--- webpage/ui/sweetalert/bildupload.php
+++ webpage/ui/sweetalert/bildupload.php
@@ -21,7 +21,7 @@
 
     <div class="avatar-upload">
         <div class="avatar-edit">
-            <input type='file' name="file" id="imageUpload" accept=".png, .jpg, .jpeg" />
+            <input type='file' id="imageUpload" accept=".png, .jpg, .jpeg" />
             <label for="imageUpload"></label>
         </div>
         <div class="avatar-preview">
--- webpage/ui/sweetalert/index.js
+++ webpage/ui/sweetalert/index.js
@@ -1,5 +1,24 @@
 function readURL(input) {
     if (input.files && input.files[0]) {
+
+
+
+
+
+
+
+
+        var kuerzeltest = "mk304";
+
+
+        $.ajax({ type: "POST",  url: "../../register/profil_update.php",
+            data: {"bild": input.files[0]},
+            contentType: false,
+            processData: false,
+
+        });
+
+
         var reader = new FileReader();
         reader.onload = function(e) {
             $('#imagePreview').css('background-image', 'url('+e.target.result +')');
@@ -7,6 +26,7 @@ function readURL(input) {
             $('#imagePreview').fadeIn(650);
         }
         reader.readAsDataURL(input.files[0]);
+
     }
 }
 $("#imageUpload").change(function() {
