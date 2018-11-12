200de8f9a1ca99833c2e0825927643985e9b676a
Moritz Klug <mk304@hdm-stuttgart.de>
2018-11-12 16:52:23 +0100

    upload

--- webpage/ui/sweetalert/index.js
+++ webpage/ui/sweetalert/index.js
@@ -7,18 +7,6 @@ function readURL(input) {
 
 
 
-
-        var kuerzeltest = "mk304";
-
-
-        $.ajax({ type: "POST",  url: "../../register/profil_update.php",
-            data: {"bild": input.files[0]},
-            contentType: false,
-            processData: false,
-
-        });
-
-
         var reader = new FileReader();
         reader.onload = function(e) {
             $('#imagePreview').css('background-image', 'url('+e.target.result +')');
