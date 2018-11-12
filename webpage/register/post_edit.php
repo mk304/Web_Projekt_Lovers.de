9792e211dc3030c49ab6468080425751992409ae
carolinsommer <40471072+carolinsommer@users.noreply.github.com>
2018-11-12 19:11:11 +0100

    Posts ändern - Datenbankverbindung

---
+++ webpage/register/post_edit.php
@@ -0,0 +1,23 @@
+<?php
+include_once 'userdata.php';
+session_start();
+
+$post = $_POST["post"];
+$id = $_POST["post_id"];
+
+$pdo = new PDO ($dsn, $dbuser, $dbpass, array('charset'=>'utf8'));
+
+$sql = "UPDATE posts SET post=:post WHERE posts_id=:id";
+$stmt=$pdo->prepare($sql);
+if (!$stmt){
+    echo "Prepare Fehler.";
+}
+//echo $pdo->query();
+
+
+if (!$stmt->execute(array( ':post'=>$post, ':id'=>$id))) {
+    echo "Query Fehler.";
+}
+
+
+?>
