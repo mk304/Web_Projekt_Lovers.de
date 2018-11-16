0c885bae8e9ec1486d3720c78593dd2f0cfabaad
Lucie Jürgens <lj025@hdm-stuttgart.de>
2018-11-16 15:27:04 +0100

    versuch divs aus und einzublenen (noch in Arbeit)

--- webpage/home/Startseite.php
+++ webpage/home/Startseite.php
@@ -46,116 +46,134 @@ if ($_SESSION["log"] == "TRUE") {
                                 </div>
                                 <hr>
                             </div>
-                            <div class="panel-body">
-                                <div class="row">
-                                    <div class="col-lg-12">
 
-                                        <form id="register-form" class="dynamic-content"
-                                              action="../register/register.php"
-                                              method="post" role="form" style="display: none;">
-                                            <div class="form-group">
-                                                <input type="text" name="kuerzel" placeholder="HdM Kürzel eingeben"
-                                                       required="required"
-                                                       minlength="5" maxlength="5"><br>
-                                            </div>
-                                            <div class="form-group">
-                                                <input type="text" name="vorname" placeholder="Vorname"
-                                                       required="required" minlength="2"
-                                                       maxlength="20"><br>
-                                            </div>
+                            <!-- Formular Registreirung -->
+                            <form action="../register/register.php" method="post">
+                                <div class="panel-body">
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <div id="register-form"
+                                                 role="form" style="display: none;">
+                                                <div class="form-group">
+                                                    <input type="text" name="kuerzel" placeholder="HdM Kürzel eingeben"
+                                                           minlength="5" maxlength="5"><br>
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="text" name="vorname" placeholder="Vorname"
+                                                           minlength="2"
+                                                           maxlength="20"><br>
+                                                </div>
 
-                                            <div class="form-group">
-                                                <input type="text" name="nachname" placeholder="Nachname"
-                                                       required="required" minlength="2"
-                                                       maxlength="20"><br>
+                                                <div class="form-group">
+                                                    <input type="text" name="nachname" placeholder="Nachname"
+                                                           minlength="2"
+                                                           maxlength="20"><br>
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="email" name="email" placeholder="E-Mail"
+                                                           minlength="2"
+                                                           maxlength="40"><br>
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="password" name="pw"
+                                                           placeholder="Passwort festlegen"
+                                                           minlength="2"
+                                                           maxlength="20">
+                                                </div>
+                                                <div class="form-group">
+                                                    <div class="row">
+                                                        <div class="col-sm-6 col-sm-offset-3">
+                                                            <input type="submit" name="register-submit"
+                                                                   id="register-submit"
+                                                                   tabindex="4" class="form-control btn btn-login"
+                                                                   value="Weiter"
+                                                                   href="Startseite.php?register-submit=Weiter"
+                                                                   onclick="sessionStorage.setItem('kuerzel')">
+                                                        </div>
+
+                                                    </div>
+                                                </div>
                                             </div>
-                                            <div class="form-group">
-                                                <input type="email" name="email" placeholder="E-Mail"
-                                                       required="required" minlength="2"
-                                                       maxlength="40"><br>
-                                            </div>
-                                            <div class="form-group">
-                                                <input type="password" name="pw"
-                                                       placeholder="Passwort festlegen" required="required"
-                                                       minlength="2"
-                                                       maxlength="20">
-                                            </div>
+                                        </div>
+
+                                        <!-- zweite div-->
+                                        <div class="dynamic-content" style="display: none" id="register-form-seite2">
                                             <div class="form-group">
                                                 <div class="row">
                                                     <div class="col-sm-6 col-sm-offset-3">
                                                         <input type="submit" name="register-submit" id="register-submit"
                                                                tabindex="4" class="form-control btn btn-login"
-                                                               value="Registrieren"
-                                                               onclick="sessionStorage.setItem('kuerzel');">
+                                                               value="Weiter"
+                                                               href="Startseite.php?register-submit=Weiter"
+                                                               onclick="sessionStorage.setItem('kuerzel');addUrl()">
                                                     </div>
 
                                                 </div>
                                             </div>
-                                        </form>
-                                        <form id="login-form" class="dynamic-content"
-                                              action="../register/login_check.php"
-                                              method="post"
-                                              role="form" style="display: none;">
-                                            <div class="form-group">
-                                                <input type="text" name="kuerzel" placeholder="HdM Kürzel eingeben"
-                                                       required="required"
-                                                       minlength="5" maxlength="5"><br>
-                                            </div>
-                                            <div class="form-group">
-                                                <input type="password" name="pw" placeholder="Passwort eingeben"
-                                                       required="required" minlength="2"
-                                                       maxlength="20"></div>
-                                            <div class="form-group">
-                                                <div class="row">
-                                                    <div class="col-sm-6 col-sm-offset-3">
-                                                        <input type="submit" name="login-submit" id="login-submit"
-                                                               tabindex="4" class="form-control btn btn-login"
-                                                               value="Log In"
-                                                               onclick="sessionStorage.setItem('kuerzel');">
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <div class="row">
-                                                    <div class="col-lg-12">
-                                                        <div class="text-center">
-                                                            <a href="https://phpoll.com/recover" tabindex="5"
-                                                               class="forgot-password">Forgot Password?</a>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </form>
-                                        <div id="register-form-seite2">
-                                        <form class="dynamic-content" style="display: none" id="register-form-2" action="#">
-                                            <div class="form-group">
-                                                <div class="col-sm-6 col-sm-offset-3">
-                                                    <input type="submit" name="weiter-submit" href="Startseite.php?seite=register-form-seite3"
-                                                           tabindex="4" class="form-control btn btn-login"
-                                                           value="Weiter"
-                                                           onclick="sessionStorage.setItem('kuerzel');">
-                                                </div>
-                                                </div>
-                                           <div>
-                                        </form>
                                         </div>
-                                        <form class="dynamic-content" style="display: none" id="register-form-seite3"
-                                              action="#"
-
-                                        </form>
-
-
                                     </div>
                                 </div>
+                            </form>
+                        </div>
 
+
+                        <form id="login-form" class="dynamic-content"
+                              action="../register/login_check.php"
+                              method="post"
+                              role="form" style="display: none;">
+                            <div class="form-group">
+                                <input type="text" name="kuerzel" placeholder="HdM Kürzel eingeben"
+                                       required="required"
+                                       minlength="5" maxlength="5"><br>
                             </div>
-
+                            <div class="form-group">
+                                <input type="password" name="pw" placeholder="Passwort eingeben"
+                                       required="required" minlength="2"
+                                       maxlength="20"></div>
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col-sm-6 col-sm-offset-3">
+                                        <input type="submit" name="login-submit" id="login-submit"
+                                               tabindex="4" class="form-control btn btn-login"
+                                               value="Log In"
+                                               onclick="sessionStorage.setItem('kuerzel');">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div class="text-center">
+                                            <a href="https://phpoll.com/recover" tabindex="5"
+                                               class="forgot-password">Forgot Password?</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                        <div id="register-form-seite2">
+                            <form class="dynamic-content" style="display: none" id="register-form-2" action="#">
+                                <div class="form-group">
+                                    <div class="col-sm-6 col-sm-offset-3">
+                                        <input type="submit" name="weiter-submit"
+                                               href="Startseite.php?seite=register-form-seite3"
+                                               tabindex="4" class="form-control btn btn-login"
+                                               value="Weiter"
+                                               onclick="sessionStorage.setItem('kuerzel');">
+                                    </div>
+                                </div>
+                                <div>
+                            </form>
                         </div>
                     </div>
                 </div>
+
+                </form>
+
             </div>
         </div>
     </div>
+
 
 
     <script>
@@ -188,45 +206,16 @@ if ($_SESSION["log"] == "TRUE") {
 
         });
 
-        // Paramter an Url für jedes Form
-
-        function getParameterByName(name, url) {
-            if (!url) url = window.location.href;
-            name = name.replace(/[\[\]]/g, "\\$&");
-            var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-                results = regex.exec(url);
-            if (!results) return null;
-            if (!results[2]) return '';
-            return decodeURIComponent(results[2].replace(/\+/g, " "));
-        }
-
-        var dynamicContent = getParameterByName('seite');
-
-        //show hide content
-
         $(document).ready(function () {
-                $('#register-form').show();
-                $('#register-form-link').show();
-                $('#login-form-link').show();
+            $('#register-form').show();
+            $('#register-form-link').show();
+            $('#login-form-link').show();
             
-            // Kontrolle ob Parameter 2 ist 2
-            if (dynamicContent == 'register-form-seite2') {
-                $('#register-form-site2').show();
-                $('#register-form-2').show();
-                $('#register-form').hide();
-                $('#register-form-link').hide();
-                $('#login-form-link').hide();
-                $('#register-form-button2').show()
-            }
-            if (dynamicContent == 'register-form-seite3') {
-                $('#register-form-site3').show();
-                $('#register-form-seite2').hide();
-                $('#register-form').hide();
-                $('#register-form-link').hide();
-                $('#login-form-link').hide();
-            }
 
         });
+
+
+
 
     </script>
 
