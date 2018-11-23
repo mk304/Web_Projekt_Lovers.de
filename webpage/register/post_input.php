f02e745d1fad28d141cb44543786b930635f48e5
carolinsommer <40471072+carolinsommer@users.noreply.github.com>
2018-11-23 17:18:03 +0100

    Notification mit ID

--- webpage/register/post_input.php
+++ webpage/register/post_input.php
@@ -16,12 +16,11 @@ $statement = $pdo->prepare($sql);
 $statement->execute(array("$kuerzel", "$channel", "$post"));
 
 
-
 // neue Posts in Tabelle notification übertragen
 $pdo2 = new PDO ($dsn, $dbuser, $dbpass, array('charset'=>'utf8'));
 $sql2 = "INSERT INTO notification (post)  
-SELECT post FROM posts WHERE not exists
-(select post from notification where notification.post = posts.post) ";
+SELECT posts_id FROM posts WHERE not exists
+(select post from notification where notification.post = posts.posts_id) ";
 
 $stmt=$pdo2->prepare($sql2);
 if (!$stmt){
