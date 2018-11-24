41cb482dc26b2ac4d3e104cd023ed62d92155c20
carolinsommer <40471072+carolinsommer@users.noreply.github.com>
2018-11-24 21:33:28 +0100

    Suche: Ausgabe Skills

--- webpage/register/do_search.php
+++ webpage/register/do_search.php
@@ -18,45 +18,55 @@ if (isset($_POST['search'])) {
     $suche = $_POST['search'];
     // Suche nach Personen
     $statement = $pdo->prepare("SELECT kuerzel, vorname, nachname FROM user WHERE kuerzel=:suche OR vorname=:suche OR nachname=:suche");
-
         if ($statement->execute(array(":suche"=>"$suche", ":vorname"=>"$suche", ":nachname"=>"$suche"))) {
             if ($anzahl_treffer = $statement->rowCount() > 0){
                 echo"<h3>Personen</h3>"."<br>";
-            }
+
+                while ($row = $statement->fetch()) {
+                    $daskuerzel = $row["kuerzel"];
+                    ?>
+                    <table>
+                        <tr>
+                            <td>
+                                <?php
+                                echo "<div class='text'>";
+
+                                $file_pointer = '../profilbilder/profilbild' . ($row["kuerzel"]) . '.jpg';
+                                echo "</div><div class='profil_bild_post' ><a class='atag' href='../webpage/profil_check.php?profilname=".$row['kuerzel']."'>";
+                                if (file_exists($file_pointer)) {
+                                    echo "<img src=\"$file_pointer\">";
+                                } else {
+                                    echo "<img src=\"../profilbilder/profilbild.jpg\">";
+                                }
+                                echo "</div></div>";
+                                ?>
+                            </td>
+                            <td> <?php echo $row["vorname"] . " " . $row["nachname"] ?><br> <a
+                                        href="../webpage/profil_anderer.php?profilname=<?php echo $row["kuerzel"] ?>"><?php echo $row["kuerzel"] ?></a>
+                                <br>
+                                <a>Skills: <?php
+                                            $statement_skills = $pdo->prepare("SELECT skill FROM skills WHERE id = ANY (SELECT skill FROM user_skills WHERE kuerzel=:kuerzel)");
+                                            $statement_skills->execute(array(":kuerzel" => "$daskuerzel"));
+
+                                            while ($row_skill = $statement_skills->fetch()) {
+                                                echo $row_skill["skill"].", ";
+                                            } ?>
+                                </a>
+                            </td>
+                        </tr>
+                    </table>
+                    <hr/>
+                    <?php
+
+
+            }}
             else {
-                echo "Ihre Suche hat leider keinen Treffer ergeben.";
+                echo "Deine Suche hat leider keinen Treffer ergeben.";
 
             }
 
-        while ($row = $statement->fetch()) {
-          ?>
-          <table>
-              <tr><td>
-                      <?php
-                      echo"<div class='text'>";
 
-                      $file_pointer = '../profilbilder/profilbild'.($row->kuerzel).'.jpg';
-                      echo"</div><div class='profil_bild_post' ><a class='atag' href='../webpage/profil_check.php?profilname=$row->kuerzel'>";
-                      if (file_exists($file_pointer))
-                      {
-                          echo "<img src=\"$file_pointer\">";
-                      }
-                      else
-                      {
-                          echo "<img src=\"../profilbilder/profilbild.jpg\">";
-                      }
-                      echo "</div></div>";
-                      ?>
-                  </td>
-                  <td> <?php echo $row["vorname"]." ".$row["nachname"]?><br> <a href="../webpage/profil_anderer.php?profilname=<?php $row["kuerzel"] ?>"><?php echo $row["kuerzel"]?></a>
-                     <br> <a>Skills: </a>
-                  </td>
 
-              </tr>
-          </table>
-            <hr/>
-        <?php
-            }
     }}
 
 
