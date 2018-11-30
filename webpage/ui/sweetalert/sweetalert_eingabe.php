7359a1ec1f3bc3e96af391b1c1cb9d7263ccd5c8
Lucie Jürgens <lj025@hdm-stuttgart.de>
2018-11-30 09:25:47 +0100

    bildupload mit ajax

--- webpage/ui/sweetalert/sweetalert_eingabe.php
+++ webpage/ui/sweetalert/sweetalert_eingabe.php
@@ -7,45 +7,62 @@
     <link rel="stylesheet" href="sweetalert2.min.css">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+
+    <title>Webslesson Tutorial | Upload File without using Form Submit in Ajax PHP</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-
-<button type="button" id="new-btn" class="btn btn-primary" >Beitrag Erstellen</button>
-<div id="result"></div>
-<script>
-
-
-    $(document).ready(function () {
-        $('#new-btn').click(function () {
-
-            (async function getText () {
-                const {value: text} = await swal({
-
-                    input: 'textarea',
-                    inputPlaceholder: 'Schreibe deine Nachricht hier...',
-                    showCancelButton: true
-                });
-                if (text) {
-                    $.ajax({ type: "POST",  url: "../register/post_input.php", data: {"post":text, "kuerzel": kuerzel, "channel": channel, "status": status},
-
-                    });
-                    swal(
-                        "Super!",
-                        "Dein Beitrag wurde erfolgreich gespeichert!",
-                        "success"
-
-
-                    )
-                    window.location.reload();
-                }
-            })()
-        });
-    })
-
-</script>
-
-
-
-
+<br /><br />
+<div class="container" style="width:700px;">
+    <h2 align="center">Upload File without using Form Submit in Ajax PHP</h2>
+    <br />
+    <label>Select Image</label>
+    <input type="file" name="file" id="file" />
+    <br />
+    <span id="uploaded_image"></span>
+</div>
 </body>
 </html>
+
+<script>
+    $(document).ready(function(){
+        $(document).on('change', '#file', function(){
+            var name = document.getElementById("file").files[0].name;
+            var form_data = new FormData();
+            var ext = name.split('.').pop().toLowerCase();
+            if(jQuery.inArray(ext, ['gif','png','jpg','jpeg']) == -1)
+            {
+                alert("Invalid Image File");
+            }
+            var oFReader = new FileReader();
+            oFReader.readAsDataURL(document.getElementById("file").files[0]);
+            var f = document.getElementById("file").files[0];
+            var fsize = f.size||f.fileSize;
+            if(fsize > 2000000)
+            {
+                alert("Image File Size is very big");
+            }
+            else
+            {
+                form_data.append("file", document.getElementById('file').files[0]);
+                $.ajax({
+                    url:"upload.php",
+                    method:"POST",
+                    data: form_data,
+                    contentType: false,
+                    cache: false,
+                    processData: false,
+                    beforeSend:function(){
+                        $('#uploaded_image').html("<label class='text-success'>Image Uploading...</label>");
+                    },
+                    success:function(data)
+                    {
+                        $('#uploaded_image').html(data);
+                    }
+                });
+            }
+        });
+    });
+</script>
