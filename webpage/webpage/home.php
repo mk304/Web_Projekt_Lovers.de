46db05a9736dce2f140de41b72feffe4af9067e0
Moritz Klug <mk304@hdm-stuttgart.de>
2018-12-06 15:04:47 +0100

    Profilbild wird im General angezeigt

--- webpage/webpage/home.php
+++ webpage/webpage/home.php
@@ -1,28 +1,18 @@
 <?php
-
-
 include_once '../ui/neuerheader.php';
 include_once '../../userdata.php';
 session_start();
-
-
 ?>
 <link rel="stylesheet" href="posts.css">
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-
 <div class="container" style=" min-width: 100%; max-height: 95vh; overflow: auto;">
     <div class="row">
         <div class="col-md-8">
-
             <br>
             <ul class="alleposts " style="min-width: 80vmax" >
-
-
                 <?php
                 $channel = $_GET["channel"];
                 $kuerzel = $_SESSION["kuerzel"];
-
-
                 //Channel auch als headline
                 $pdo = new PDO ($dsn, $dbuser, $dbpass, array('charset' => 'utf8'));
                 $sql_3 = "SELECT name from channels WHERE channel_id=:channel ";
@@ -31,12 +21,10 @@ session_start();
                 while ($row = $query_3->fetchObject()) {
                     echo "<h2>$row->name</h2>";
                 }
-
                     // Channel "General" als Startseite definieren
                     // Nur Posts von Personen, denen der Eingeloggte folgt werden ausgespielt
                     if ($channel == "") {
                         echo "<h2>General</h2>";
-
                         $pdo = new PDO ($dsn, $dbuser, $dbpass);
                         $sql = "SELECT  post, kuerzel, date, posts_id, bild_id from posts WHERE kuerzel = ANY (SELECT folgt FROM abonnenten WHERE kuerzel = :kuerzel) ORDER BY posts.date DESC";
                         $query = $pdo->prepare($sql);
@@ -45,40 +33,24 @@ session_start();
 
                             if ($row->bild_id == NULL){
                                 echo "<div class='inhalt'>";
-
                                 echo "<div class='text'>";
-
                                 echo "<a ><h3>" . ($zeile->post) . "</h3><br><h4>" . " schrieb <a   href='../webpage/profil_check.php?profilname=$zeile->kuerzel'>" . ($zeile->kuerzel) . "</a> um " . date('g:i a, F j, Y', strtotime($zeile->date));
                                 echo "</h4>";
                             }
-
-
                             if ($zeile->post == NULL) {
-
-
-
                                 $bildlink= $zeile-> bild_id;
-
-
                                 echo "<a href='../bildupload/$bildlink'><img class='bild' src='../bildupload/$bildlink'>";
                                 echo ($zeile->post);
-
                             }
-
-                            $file_pointer = '../profilbilder/profilbild' . ($row->kuerzel) . '.jpg';
-                            echo "</div><div class='profil_bild_post' ><a class='atag' href='../webpage/profil_check.php?profilname=$row->kuerzel'>";
-
-
-
+                            $file_pointer = '../profilbilder/profilbild' . ($zeile->kuerzel) . '.jpg';
+                            echo "</div><div class='profil_bild_post' ><a class='atag' href='../webpage/profil_check.php?profilname=$zeile->kuerzel'>";
                             if (file_exists($file_pointer)) {
                                 echo "<img src=\"$file_pointer\">";
                             } else {
                                 echo "<img src=\"../profilbilder/profilbild.jpg\">";
                             }
                             echo "</div></div>";
-
                         }
-
                     }
                 //Posts aus Channel ausgeben
 
@@ -88,38 +60,26 @@ session_start();
                 $query_3->execute(array(":channel" => "$channel"));
 
                 while ($row = $query_3->fetchObject()) {
-
                     // Ausgabe der Textposts
                     if ($row->bild_id == NULL){
                         echo "<div class='inhalt'>";
-
                         echo "<div class='text'>";
-
                         echo "<a ><h3>" . ($row->post) . "</h3><br><h4>" . " schrieb <a   href='../webpage/profil_check.php?profilname=$row->kuerzel'>" . ($row->kuerzel) . "</a> um " . date('g:i a, F j, Y', strtotime($row->date));
                         echo "</h4>";
                         if (($row->kuerzel) == $kuerzel) {
                             echo "<button class='download'  onClick='sessionStorage.id=$row->posts_id'>Post bearbeiten</button>";
                             echo "<a class='post_bearbeiten2'  href='../register/do_post_delete.php?id=$row->posts_id'>Post löschen</>";
-
                         }
                     }
-
                     // Ausgabe der Bildposts
                     if ($row->post == NULL) {
-
                         echo "<div class='inhalt'>";
-
                         echo "<div class='text' >";
                         $bildlink= $row-> bild_id;
-
-
                         echo "<a href='../bildupload/$bildlink'><img class='bild' src='../bildupload/$bildlink'>";
                     }
-
                     $file_pointer = '../profilbilder/profilbild' . ($row->kuerzel) . '.jpg';
                     echo "</div><div class='profil_bild_post' ><a class='atag' href='../webpage/profil_check.php?profilname=$row->kuerzel'>";
-
-
 
                     if (file_exists($file_pointer)) {
                         echo "<img src=\"$file_pointer\">";
@@ -128,14 +88,10 @@ session_start();
                     }
                     echo "</div></div>";
                     ?>
-
-
                     <script>
                         var post_id = sessionStorage.getItem('id');
-
                         $(document).ready(function () {
                             $('.download').click(function () {
-
                                 (async function getText() {
                                     const {value: text} = await swal({
                                         input: 'textarea',
@@ -162,18 +118,11 @@ session_start();
                     </script>
                     <?php
                 }
-
-
-
                 ?>
-
             </ul>
-
         </div>
     </div>
 </div>
-
-
 <?php
 include_once '../ui/footer.php';
 
