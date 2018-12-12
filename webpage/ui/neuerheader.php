6c9803431257e4480df6e27968db7eb3558bbccc
Lucie Jürgens <lj025@hdm-stuttgart.de>
2018-12-12 14:29:19 +0100

    icon ersetzt für navbar

--- webpage/ui/neuerheader.php
+++ webpage/ui/neuerheader.php
@@ -166,7 +166,7 @@ $kuerzel = $_SESSION["kuerzel"];
 
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
                         aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                    <span class="fas fa-ellipsis-h"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarsExampleDefault">
