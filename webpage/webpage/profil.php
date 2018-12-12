bd39a0897bf3b058b111ed7d7a4feff5b7d54a3f
Lucie Jürgens <lj025@hdm-stuttgart.de>
2018-12-12 14:47:07 +0100

    warung mit sweet alert "Datei zu groß " bildupload

--- webpage/webpage/profil.php
+++ webpage/webpage/profil.php
@@ -8,6 +8,10 @@ $kuerzel = $_SESSION["kuerzel"];
 $profilname = $_GET["profilname"];
 
 
+if ($_GET['seite'] == "warning_dateiformat") {
+    $message = "wrong answer";
+    echo "<script type='text/javascript'>swal('Deine Datei ist zu groß! (Max Größe 1MB)');</script>";
+}
 ?>
     <link rel="stylesheet" href="style.css"> <link rel="stylesheet" href="posts.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
